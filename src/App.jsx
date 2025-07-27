ff610bb3248b1ff9dbe0fa7a4609b0b2521f6975
HashenWatawala <hashanewatawala@gmail.com>
2025-07-27 18:14:14 +0530

    bot answer provide in UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,44 @@
-import React, { useState } from "react";
+import React, {useRef, useState } from "react";
 import Chatboticon from "./components/Chatboticon";
 import ChatForm from "./components/ChatForm";
 import ChatMessage from "./components/ChatMessage";
 
 const App = () => {
-  const[chatHistory, setChatHistory] = useState([]);
-
-  const generateBotResponse = async (history) =>{
-
-    history = history.map(({role, text}) => ({role, parts:[{text}]}))
+  const [chatHistory, setChatHistory] = useState([]);
+  const chatBodyRef = useRef();
+  const generateBotResponse = async (history) => {
+    const updateHistory = (text) => {
+      setChatHistory(prev => [
+        ...prev.filter(msg => msg.text !== "Thinking..."),
+        { role: "model", text }
+      ]);
+    };
+    history = history.map(({ role, text }) => ({ role, parts: [{ text }] }));
     const requestOptions = {
       method: "POST",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify({contents: history})
-    }
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ contents: history }),
+    };
 
-    try{
-      const response = await fetch(import.meta.env.VITE_API_KEY, requestOptions);
+    try {
+      const response = await fetch(
+        import.meta.env.VITE_API_KEY,
+        requestOptions
+      );
       const data = await response.json();
-      if(! response.ok) throw new Error(data.error.message || "Something went wrong");
+      if (!response.ok)
+        throw new Error(data.error.message || "Something went wrong");
 
-      console.log(data)
-    } catch(error){
+      const apiResponseText = data.candidates[0].content.parts[0].text
+        .replace(/\*\*(.*?)\*\*/g, "$1")
+        .trim();
+      updateHistory(apiResponseText);
+    } catch (error) {
       console.log(error);
     }
   };
+
+  use
   return (
     <div className="container">
       <div className="chatbot-popup">
@@ -34,20 +48,29 @@ const App = () => {
             <Chatboticon />
             <h2 className="logo-text">ChatBot</h2>
           </div>
-          <button className="material-symbols-outlined">keyboard_arrow_down</button>
+          <button className="material-symbols-outlined">
+            keyboard_arrow_down
+          </button>
         </div>
         {/*Chatbot body*/}
-        <div className="chat-body">
+        <div ref={chatBodyRef} className="chat-body">
           <div className="message bot-message">
-            <Chatboticon/>
+            <Chatboticon />
             <p className="message-text">
-              Hey there👋 <br/>How can I help you today?
+              Hey there👋 <br />
+              How can I help you today?
             </p>
           </div>
-          {chatHistory.map((chat, index)=>(<ChatMessage key={index} chat={chat}/>))}
+          {chatHistory.map((chat, index) => (
+            <ChatMessage key={index} chat={chat} />
+          ))}
         </div>
         <div className="chat-footer">
-          <ChatForm setChatHistory={setChatHistory} generateBotResponse={generateBotResponse} chatHistory={chatHistory}/>
+          <ChatForm
+            setChatHistory={setChatHistory}
+            generateBotResponse={generateBotResponse}
+            chatHistory={chatHistory}
+          />
         </div>
       </div>
     </div>
